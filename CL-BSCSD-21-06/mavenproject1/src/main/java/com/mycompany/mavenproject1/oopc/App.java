b45e51d5c4d8c6d74655cdbc724717c3bc9299d5
sesathdissanayake <sesathdissanayake@gmail.com>
2020-12-06 09:48:17 +0530

    refactor pkg and classes

--- CL-BSCSD-21-06/mavenproject1/src/main/java/com/mycompany/mavenproject1/oopc/App.java
+++ CL-BSCSD-21-06/mavenproject1/src/main/java/com/mycompany/mavenproject1/oopc/App.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mavenproject1;
+package com.mycompany.mavenproject1.oopc;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -12,8 +15,23 @@ package com.mycompany.mavenproject1;
 public class App {
 
     public static void main(String[] args) {
-        Student student = new Student("ST0001", "John", "Smith");
-        System.out.println(student.getFullName());
+        //        Student student = new Student("ST0001", "John", "Smith");
+        //        Employee employee = new Employee("EM0001", "Mark", "Rider");
+        //        System.out.println(student.getFullName());
+        //        System.out.println(employee.getFullName());
+
+        // polymorphism
+        Person student1 = new Student("ST0001", "Leonardo", "Turtle");
+        Person student2 = new Student("ST0002", "Raphael", "Turtle");
+        Person student3 = new Student("ST0003", "Donatello", "Turtle");
+        Person student4 = new Student("ST0004", "Michelangelo", "Turtle");
+        Person employee1 = new Employee("EM0001", "Mark", "Rider");
+
+        List<Person> personCollection = Arrays.asList(student1, student2, student3, student4, employee1);
+
+        personCollection.forEach((Person person) -> {
+            System.out.println(person.getFullName());
+        });
 
     }
 }
